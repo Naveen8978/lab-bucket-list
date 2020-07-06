e081f8f755a706bd308f516b9e053b30a86bd60d
@naveen8978 <naveengajja27@gmail.com>
2020-07-06 07:26:46 +0530

    ProGrad 007

--- src/service/ListOperations.java
+++ src/service/ListOperations.java
@@ -1,1 +1,13 @@
 package service;
+public class ListOperations{
+	List<TouristPlace> list=new ArrayList<TouristPlace>();
+	public List add(TouristPlace places)
+	{
+
+		list.add(places);
+		return list;
+
+
+	}
+	public List remove(TouristPlace places)
+	
